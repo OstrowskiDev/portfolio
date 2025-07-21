f7c1728087a4ad307f17907a2ce148592ff6b051
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2025-07-21 22:27:07 +0200

    Initial commit.

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="w-20 h-20 bg-red-500"></div>
+    </main>
+  )
+}
