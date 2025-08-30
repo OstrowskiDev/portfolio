3efef5a74941938754eb727c284dcb59c8fcec40
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2025-08-30 19:50:17 +0200

    Changed nextjs.yml workflow to now be also triggered automatically on push to repo. Also deleted Intro.tsx, its logic was moved to other files. Added better visual style to NavBar.

--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -2,7 +2,7 @@ import { NavLink } from '../common/NavLink'
 
 export default function NavBar() {
   return (
-    <div className="navbar sticky top-0 z-[100] w-full h-[100px] flex justify-around items-center bg-white">
+    <div className="navbar sticky top-0 z-[100] w-full h-[100px] flex justify-around items-center border-b-[1px] border-primary-200 shadow-[0_2px_2px_theme(colors.primary.100)] bg-white">
       <NavLink name="Intro" targetId="intro" />
       <NavLink name="Home" targetId="home" />
       <NavLink name="Portfolio" targetId="portfolio" />
