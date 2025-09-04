2336736fb341a936ae606acd10b8dd521b08b44d
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2025-09-04 17:38:30 +0200

    Added wide version of ProjectCard for UI/UX tests.

---
+++ src/components/cards/ProjectCardWide.tsx
@@ -0,0 +1,51 @@
+import { ProjectData } from '@/types/ProjectDada'
+import TechStackItem from './TechStackItem'
+import ExternalLinksSection from './ExternalLinksSection'
+import ProjectCardHeader from './ProjectCardHeader'
+import ChallengesSection from './ChallengesSection'
+import LearnedSection from './LearnedSection'
+
+export default function ProjectCardWide({
+  projectData,
+}: {
+  projectData: ProjectData
+}) {
+  return (
+    <div
+      className="project-card-container relative top-[80px] w-full h-[calc(100vh-80px)] overflow-hidden font-inter border-[12px] border-r-0"
+      style={{
+        borderColor: projectData.bgColor,
+        backgroundColor: projectData.bgColor,
+        backgroundImage: `url('/projects/${projectData.image}')`,
+        backgroundSize: 'auto 100%',
+        backgroundPosition: 'top left',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
+      <div className="project-card-content absolute top-[calc((100%-660px)/2)] left-[calc(50vw-290px)] flex flex-col w-[580px] h-[640px] px-6 pt-4 bg-white rounded-xl shadow-[0_0_45px_rgba(0,0,0,.9)]">
+        <ProjectCardHeader
+          title={projectData.title}
+          subtitle={projectData.subtitle}
+        />
+
+        <ExternalLinksSection links={projectData.links} />
+
+        <ChallengesSection challenges={projectData.challenges} />
+
+        <LearnedSection learned={projectData.learned} />
+
+        <div className="project-tech-stack-container flex flex-wrap justify-between mt-auto w-full h-[55px] pt-1 border-t border-primary-300">
+          {projectData.techStack.map((tech) => {
+            return (
+              <TechStackItem
+                text={tech.text}
+                ico={tech?.ico}
+                noIcon={tech?.noIcon}
+              />
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
