0b75ba60a1956433554f217440f7678caecf3402
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2026-07-28 01:30:59 +0200

    build(next.js): add webpack rule that allows imports of .md files as raw strings
    This will be later used to build article objects. Article.content is better kept inside pure .md files for QoL during edits and formatting.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  webpack: function (config) {
+    config.module.rules.push({
+      test: /\.md$/, // match only .md import files
+      type: 'asset/source', // import as raw string
+    })
+    return config
+  },
 }
 
 export default nextConfig
