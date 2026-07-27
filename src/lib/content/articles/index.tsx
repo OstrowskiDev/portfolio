ccdd21d2645768ac9782221e285bb6d003018c12
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2026-07-28 01:40:05 +0200

    feat(articles): add centralized export file for all articles

---
+++ src/lib/content/articles/index.tsx
@@ -0,0 +1,5 @@
+import { Article } from '@/types'
+import { article as article01 } from './blueprints-that-build-themselves'
+import { article as article02 } from './from-waiting-to-building'
+
+export const articles: Article[] = [article01, article02]
