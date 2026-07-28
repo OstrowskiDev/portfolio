3c6f574d4b6e50824945e0ce4efca99840faa51d
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2026-07-29 01:38:39 +0200

    refactor(CarouselDots): delegate positioning to parent flex layout

--- src/components/carousel/CarouselDots.tsx
+++ src/components/carousel/CarouselDots.tsx
@@ -8,14 +8,14 @@ export default function CarouselDots({
   currentIndex,
 }: CarouselIndicatorProps) {
   return (
-    <div className="carousel-dots absolute bottom-12 left-1/2 transform -translate-x-1/2 flex items-center gap-5 z-20">
+    <div className="carousel-dots flex items-center gap-5 mb-16">
       {Array.from({ length: totalItems }, (_, index) => (
         <div
           key={index}
           className={`dot transition-all duration-500 ease-in-out ${
             index === currentIndex
-              ? 'w-4 h-4 bg-primary-100'
-              : 'w-3 h-3  bg-primary-300'
+              ? 'w-4 h-4 bg-primary-400'
+              : 'w-3 h-3 bg-primary-300'
           }`}
         />
       ))}
