6f02bf654dbcc801ed3d558bf764065395c41010
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2026-07-29 01:30:05 +0200

    chore(ts): add types for extras section

---
+++ src/types/extra.ts
@@ -0,0 +1,12 @@
+export type Links = {
+  github: string
+  youtube?: string
+  article?: string
+}
+
+export type Extra = {
+  title: string
+  description: string
+  highlights: string[]
+  links: Links
+}
