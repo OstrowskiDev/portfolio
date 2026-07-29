75fd1874c4509bb71a157ce0d9334052d9afb79d
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2026-07-29 19:35:42 +0200

    feat(pagination): add pagination components for articles and extras sections

---
+++ src/components/pagination/Pagination.tsx
@@ -0,0 +1,52 @@
+import PaginationArrow from './PaginationArrow'
+
+interface PaginationProps {
+  totalItems: number
+  currentIndex: number
+  goToPrevious: () => void
+  goToNext: () => void
+  sectionLabel: string
+}
+
+export default function Pagination({
+  totalItems,
+  currentIndex,
+  goToPrevious,
+  goToNext,
+  sectionLabel,
+}: PaginationProps) {
+  return (
+    <div
+      className={`${sectionLabel}-pagination-container flex flex-row justify-center items-center mb-16`}
+    >
+      <PaginationArrow
+        size={32}
+        direction="left"
+        sectionLabel={sectionLabel}
+        onClick={goToPrevious}
+        isActive={currentIndex > 0}
+      />
+
+      <div className="pagination-dots flex items-center gap-5 ">
+        {Array.from({ length: totalItems }, (_, index) => (
+          <div
+            key={index}
+            className={`dot transition-all duration-500 ease-in-out ${
+              index === currentIndex
+                ? 'w-4 h-4 bg-primary-400'
+                : 'w-3 h-3 bg-primary-300'
+            }`}
+          />
+        ))}
+      </div>
+
+      <PaginationArrow
+        size={32}
+        sectionLabel={sectionLabel}
+        direction="right"
+        onClick={goToNext}
+        isActive={currentIndex < totalItems - 1}
+      />
+    </div>
+  )
+}
---
+++ src/components/pagination/PaginationArrow.tsx
@@ -0,0 +1,38 @@
+import ChevronIco from '../icons/ChevronIco'
+
+interface PaginationArrowProps {
+  size: number
+  sectionLabel: string
+  direction: 'left' | 'right'
+  onClick: () => void
+  isActive: boolean
+}
+
+export default function PaginationArrow({
+  size,
+  sectionLabel,
+  direction,
+  onClick,
+  isActive,
+}: PaginationArrowProps) {
+  return (
+    <button
+      onClick={onClick}
+      aria-label={`${direction === 'left' ? 'Previous' : 'Next'} ${sectionLabel}`}
+      aria-controls={sectionLabel}
+      tabIndex={0}
+      className={`
+        p-3 
+        transition-all duration-200 z-20
+        hover:scale-110
+        ${isActive ? 'text-primary-400 hover:text-primary-500' : 'text-primary-200 cursor-default'}
+`}
+    >
+      <ChevronIco
+        width={size}
+        height={size}
+        className={direction === 'left' ? 'rotate-180' : ''}
+      />
+    </button>
+  )
+}
