6a09d027879071b474c74305821b279ed8eb684c
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2026-08-03 22:13:39 +0200

    feat(contact): add tooltip to copy to clipboard btn

--- src/components/contact/CopyToClipboardBtn.tsx
+++ src/components/contact/CopyToClipboardBtn.tsx
@@ -5,6 +5,7 @@ import { copyToClipboard } from '@/lib/helpers'
 
 export default function CopyToClipboardBtn({ email }: { email: string }) {
   const [isCopied, setIsCopied] = useState(false)
+  const [isHovered, setIsHovered] = useState(false)
 
   async function handleCopyToClipboard() {
     const copied = await copyToClipboard(email)
@@ -15,22 +16,27 @@ export default function CopyToClipboardBtn({ email }: { email: string }) {
   }
 
   return (
-    <button
-      type="button"
-      onClick={handleCopyToClipboard}
-      className={`contact-copy-button 
-            p-2
-            rounded-xl
-            text-primary-50
-            hover:bg-primary-600
-           `}
-      aria-label="Copy email address"
-    >
-      {isCopied ? (
-        <CheckIco className="h-6 w-6" />
-      ) : (
-        <CopyIcon className="h-6 w-6" />
+    <div className="contact-copy-bnt-container relative">
+      <button
+        type="button"
+        onClick={handleCopyToClipboard}
+        className={`contact-copy-bnt p-2 rounded-xl text-primary-50  hover:bg-primary-700`}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        aria-label="Copy email address"
+      >
+        {isCopied ? (
+          <CheckIco className="h-5 w-5" />
+        ) : (
+          <CopyIcon className="h-5 w-5" />
+        )}
+      </button>
+
+      {isHovered && !isCopied && (
+        <span className="absolute top-full mt-2 px-2 py-1 left-1/2 -translate-x-1/2 text-sm font-semibold text-primary-100 bg-primary-800 rounded-xl">
+          Copy
+        </span>
       )}
-    </button>
+    </div>
   )
 }
