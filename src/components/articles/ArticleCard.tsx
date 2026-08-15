ebc35b46d0c8ffe1b7ab734688e4a53ceabfb89f
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2026-08-15 20:02:30 +0200

    fix(articles): prevent overflow to next line of articles title

--- src/components/articles/ArticleCard.tsx
+++ src/components/articles/ArticleCard.tsx
@@ -11,11 +11,11 @@ export default function ArticleCard({ article }: { article: Article }) {
       className="article group border-b flex flex-row w-full h-[140px] p-4  border-primary-300 bg-white"
     >
       <div className="article-iconography w-2 border-r-[3px] border-primary-300 group-hover:border-accent-600 transition-colors duration-400 ease-out"></div>
-      <div className="article-container flex flex-col w-full ml-4">
+      <div className="article-container flex flex-col w-full min-w-0 ml-4">
         <div className="article-tag uppercase text-accent-600 text-xs font-semibold tracking-[0.5px] group-hover:translate-x-1 transition-transform duration-200 ease-out">
           {tag}
         </div>
-        <div className="article-title font-bold mt-1 text-primary-950 text-lg group-hover:translate-x-1 transition-transform duration-200 ease-out">
+        <div className="article-title truncate font-bold mt-1 text-primary-950 text-lg group-hover:translate-x-1 transition-transform duration-200 ease-out">
           {title}
         </div>
         <div className="article-description text-sm mt-1 text-primary-500 font-semibold leading-[1.55] group-hover:text-primary-700 group-hover:translate-x-1 transition-transform duration-200 ease-out">
